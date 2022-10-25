7b42968e7aa0b90d5cb0d56b0887e94ca6a401da
kml <kml.ptb@gmail.com>
2022-10-25 20:23:25 +0100

    added counter creation function, added counter droppoints for  mobile device ui

--- js/test.js
+++ js/test.js
@@ -1,278 +1,280 @@
 let desktopAnimation = [
-    [
-      { left: "29.2%", top: " 22%" },
-      { left: "29.2%", top: " 32%" },
-      { leftt: "29.2%", top: " 41.5%" },
-      { left: "29.2%", top: " 51.5%" },
-      { leftt: "29.2%", top: " 61.2%" },
-      { left: "29.2%", top: " 71%" },
-    ],
-    [
-      { left: "35.4%", top: " 22%" },
-      { left: "35.4%", top: " 32%" },
-      { left: "35.4%", top: " 41.5%" },
-      { left: "35.4%", top: " 51.5%" },
-      { left: "35.4%", top: " 61.2%" },
-      { left: "35.4%", top: " 71%" },
-    ],
-    [
-      { left: "41.4%", top: " 22%" },
-      { left: "41.4%", top: " 32%" },
-      { leftt: "41.4%", top: " 41.5%" },
-      { left: "41.4%", top: " 51.5%" },
-      { left: "41.4%", top: " 61.2%" },
-      { left: "41.4%", top: " 71%" },
-    ],
-    [
-      { left: "47.4%", top: " 22%" },
-      { left: "47.4%", top: " 32%" },
-      { left: "47.4%", top: " 41.5%" },
-      { left: "47.4%", top: " 51.5%" },
-      { left: "47.4%", top: " 61.2%" },
-      { left: "47.4%", top: " 71%" },
-    ],
-    [
-      { left: "53.7%", top: " 22%" },
-      { left: "53.7%", top: " 32%" },
-      { leftt: "53.7%", top: " 41.5%" },
-      { left: "53.7%", top: " 51.5%" },
-      { left: "53.7%", top: " 61.2%" },
-      { left: "53.7%", top: " 71%" },
-    ],
-    [
-      { left: "59.7%", top: " 22%" },
-      { left: "59.7%", top: " 32%" },
-      { left: "59.7%", top: " 41.5%" },
-      { left: "59.7%", top: " 51.5%" },
-      { left: "59.7%", top: " 61.2%" },
-      { left: "59.7%", top: " 71%" },
-    ],
-    [
-      { left: "65.9%", Top: " 22%" },
-      { left: "65.9%", top: " 32%" },
-      { left: "65.9%", top: " 41.5%" },
-      { left: "65.9%", top: " 51.5%" },
-      { leftt: "65.9%", top: " 61.2%" },
-      { left: "65.9%", top: " 71%" },
-    ],
-  ];
-  
-  let tabletAnimation = [
-    [
-      { left: "12.2%", top: " 31.5%" },
-      { left: "12.2%", top: " 40.1%" },
-      { left: "12.2%", top: " 49%" },
-      { left: "12.2%", top: " 57.5%" },
-      { left: "12.2%", top: " 66.2%" },
-      { left: "12.2%", top: "75%" },
-    ],
-    [
-      { left: "23.7%", top: " 31.5%" },
-      { left: "23.7%", top: " 40.1%" },
-      { left: "23.7%", top: " 49%" },
-      { left: "23.7%", top: " 57.5%" },
-      { left: "23.7%", top: " 66.2%" },
-      { left: "23.7%", top: "75%" },
-    ],
-    [
-      { leftt: "35.2%", top: " 31.5%" },
-      { leftt: "35.2%", top: " 40.1%" },
-      { leftt: "35.2%", top: " 49%" },
-      { leftt: "35.2%", top: " 57.5%" },
-      { leftt: "35.2%", top: " 66.2%" },
-      { leftt: "35.2%", top: "75%" },
-    ],
-    [
-      { left: "46.5%", top: " 31.5%" },
-      { left: "46.5%", top: " 40.1%" },
-      { left: "46.5%", top: " 49%" },
-      { left: "46.5%", top: " 57.5%" },
-      { left: "46.5%", top: " 66.2%" },
-      { left: "46.5%", top: "75%" },
-    ],
-    [
-      { left: "58.1%", top: " 31.5%" },
-      { left: "58.1%", top: " 40.1%" },
-      { left: "58.1%", top: " 49%" },
-      { left: "58.1%", top: " 57.5%" },
-      { left: "58.1%", top: " 66.2%" },
-      { left: "58.1%", top: "75%" },
-    ],
-    [
-      { left: "69.5%", top: " 31.5%" },
-      { left: "69.5%", top: " 40.1%" },
-      { left: "69.5%", top: " 49%" },
-      { left: "69.5%", top: " 57.5%" },
-      { left: "69.5%", top: " 66.2%" },
-      { left: "69.5%", top: "75%" },
-    ],
-    [
-      { left: "80.9%", top: " 31.5%" },
-      { left: "80.9%", top: " 40.1%" },
-      { left: "80.9%", top: " 49%" },
-      { left: "80.9%", top: " 57.5%" },
-      { left: "80.9%", top: " 66.2%" },
-      { left: "80.9%", top: "75%" },
-    ],
-  ];
-  
-  let mobileAnimation = [
-    [
-      { left: "6.5%", top: " 35.8%" },
-      { left: "6.5%", top: " 41.8%" },
-      { leftt: "6.5%", top: " 47.3%" },
-      { left: "6.5%", top: " 53.3%" },
-      { leftt: "6.5%", top: " 59.1%" },
-      { leftt: "6.5%", top: " 64.8%" },
-    ],
-    [
-      { left: "19.5%", top: " 35.8%" },
-      { left: "19.5%", top: " 41.8%" },
-      { left: "19.5%", top: " 47.3%" },
-      { left: "19.5%", top: " 53.3%" },
-      { left: "19.5%", top: " 59.1%" },
-      { left: "19.5%", top: " 64.8%" },
-    ],
-    [
-      { left: "31.5%", top: " 35.8%" },
-      { left: "31.5%", top: " 41.8%" },
-      { left: "31.5%", top: " 47.3%" },
-      { left: "31.5%", top: " 53.3%" },
-      { left: "31.5%", top: " 59.1%" },
-      { left: "31.5%", top: " 64.8%" },
-    ],
-    [
-      { left: "44.5%", top: " 35.8%" },
-      { left: "44.5%", top: " 41.8%" },
-      { left: "44.5%", top: " 47.3%" },
-      { left: "44.5%", top: " 53.3%" },
-      { left: "44.5%", top: " 59.1%" },
-      { left: "44.5%", top: " 64.8%" },
-    ],
-    [
-      { left: "56.5%", top: " 35.8%" },
-      { left: "56.5%", top: " 41.8%" },
-      { left: "56.5%", top: " 47.3%" },
-      { left: "56.5%", top: " 53.3%" },
-      { left: "56.5%", top: " 59.1%" },
-      { left: "56.5%", top: " 64.8%" },
-    ],
-    [
-      { left: "69.2%", top: " 35.8%" },
-      { left: "69.2%", top: " 41.8%" },
-      { left: "69.2%", top: " 47.3%" },
-      { left: "69.2%", top: " 53.3%" },
-      { left: "69.2%", top: " 59.1%" },
-      { left: "69.2%", top: " 64.8%" },
-    ],
-    [
-      { left: "81.5%", top: " 35.8%" },
-      { left: "81.5%", top: " 41.8%" },
-      { left: "81.5%", top: " 47.3%" },
-      { left: "81.5%", top: " 53.3%" },
-      { left: "81.5%", top: " 59.1%" },
-      { left: "81.5%", top: " 64.8%" },
-    ],
-  ];
+  [
+    { left: "29.2%", top: " 22%" },
+    { left: "29.2%", top: " 32%" },
+    { leftt: "29.2%", top: " 41.5%" },
+    { left: "29.2%", top: " 51.5%" },
+    { leftt: "29.2%", top: " 61.2%" },
+    { left: "29.2%", top: " 71%" },
+  ],
+  [
+    { left: "35.4%", top: " 22%" },
+    { left: "35.4%", top: " 32%" },
+    { left: "35.4%", top: " 41.5%" },
+    { left: "35.4%", top: " 51.5%" },
+    { left: "35.4%", top: " 61.2%" },
+    { left: "35.4%", top: " 71%" },
+  ],
+  [
+    { left: "41.4%", top: " 22%" },
+    { left: "41.4%", top: " 32%" },
+    { leftt: "41.4%", top: " 41.5%" },
+    { left: "41.4%", top: " 51.5%" },
+    { left: "41.4%", top: " 61.2%" },
+    { left: "41.4%", top: " 71%" },
+  ],
+  [
+    { left: "47.4%", top: " 22%" },
+    { left: "47.4%", top: " 32%" },
+    { left: "47.4%", top: " 41.5%" },
+    { left: "47.4%", top: " 51.5%" },
+    { left: "47.4%", top: " 61.2%" },
+    { left: "47.4%", top: " 71%" },
+  ],
+  [
+    { left: "53.7%", top: " 22%" },
+    { left: "53.7%", top: " 32%" },
+    { leftt: "53.7%", top: " 41.5%" },
+    { left: "53.7%", top: " 51.5%" },
+    { left: "53.7%", top: " 61.2%" },
+    { left: "53.7%", top: " 71%" },
+  ],
+  [
+    { left: "59.7%", top: " 22%" },
+    { left: "59.7%", top: " 32%" },
+    { left: "59.7%", top: " 41.5%" },
+    { left: "59.7%", top: " 51.5%" },
+    { left: "59.7%", top: " 61.2%" },
+    { left: "59.7%", top: " 71%" },
+  ],
+  [
+    { left: "65.9%", Top: " 22%" },
+    { left: "65.9%", top: " 32%" },
+    { left: "65.9%", top: " 41.5%" },
+    { left: "65.9%", top: " 51.5%" },
+    { leftt: "65.9%", top: " 61.2%" },
+    { left: "65.9%", top: " 71%" },
+  ],
+];
 
-  
-let large = document.getElementById('counter');
-let small = document.getElementById('counter1');
-let game= document.getElementsByClassName("flex-conter-board");
+let tabletAnimation = [
+  [
+    { left: "12.2%", top: " 31.5%" },
+    { left: "12.2%", top: " 40.1%" },
+    { left: "12.2%", top: " 49%" },
+    { left: "12.2%", top: " 57.5%" },
+    { left: "12.2%", top: " 66.2%" },
+    { left: "12.2%", top: "75%" },
+  ],
+  [
+    { left: "23.7%", top: " 31.5%" },
+    { left: "23.7%", top: " 40.1%" },
+    { left: "23.7%", top: " 49%" },
+    { left: "23.7%", top: " 57.5%" },
+    { left: "23.7%", top: " 66.2%" },
+    { left: "23.7%", top: "75%" },
+  ],
+  [
+    { leftt: "35.2%", top: " 31.5%" },
+    { leftt: "35.2%", top: " 40.1%" },
+    { leftt: "35.2%", top: " 49%" },
+    { leftt: "35.2%", top: " 57.5%" },
+    { leftt: "35.2%", top: " 66.2%" },
+    { leftt: "35.2%", top: "75%" },
+  ],
+  [
+    { left: "46.5%", top: " 31.5%" },
+    { left: "46.5%", top: " 40.1%" },
+    { left: "46.5%", top: " 49%" },
+    { left: "46.5%", top: " 57.5%" },
+    { left: "46.5%", top: " 66.2%" },
+    { left: "46.5%", top: "75%" },
+  ],
+  [
+    { left: "58.1%", top: " 31.5%" },
+    { left: "58.1%", top: " 40.1%" },
+    { left: "58.1%", top: " 49%" },
+    { left: "58.1%", top: " 57.5%" },
+    { left: "58.1%", top: " 66.2%" },
+    { left: "58.1%", top: "75%" },
+  ],
+  [
+    { left: "69.5%", top: " 31.5%" },
+    { left: "69.5%", top: " 40.1%" },
+    { left: "69.5%", top: " 49%" },
+    { left: "69.5%", top: " 57.5%" },
+    { left: "69.5%", top: " 66.2%" },
+    { left: "69.5%", top: "75%" },
+  ],
+  [
+    { left: "80.9%", top: " 31.5%" },
+    { left: "80.9%", top: " 40.1%" },
+    { left: "80.9%", top: " 49%" },
+    { left: "80.9%", top: " 57.5%" },
+    { left: "80.9%", top: " 66.2%" },
+    { left: "80.9%", top: "75%" },
+  ],
+];
 
-  function animate(element,frames){
-    element.animate(frames,{
-        duration:1000,
-        iterations: 1,
-        fill:'forwards',
-})
+let mobileAnimation = [
+  [
+    { left: "6.5%", top: " 35.8%" },
+    { left: "6.5%", top: " 41.8%" },
+    { leftt: "6.5%", top: " 47.3%" },
+    { left: "6.5%", top: " 53.3%" },
+    { leftt: "6.5%", top: " 59.1%" },
+    { leftt: "6.5%", top: " 64.8%" },
+  ],
+  [
+    { left: "19.5%", top: " 35.8%" },
+    { left: "19.5%", top: " 41.8%" },
+    { left: "19.5%", top: " 47.3%" },
+    { left: "19.5%", top: " 53.3%" },
+    { left: "19.5%", top: " 59.1%" },
+    { left: "19.5%", top: " 64.8%" },
+  ],
+  [
+    { left: "31.5%", top: " 35.8%" },
+    { left: "31.5%", top: " 41.8%" },
+    { left: "31.5%", top: " 47.3%" },
+    { left: "31.5%", top: " 53.3%" },
+    { left: "31.5%", top: " 59.1%" },
+    { left: "31.5%", top: " 64.8%" },
+  ],
+  [
+    { left: "44.5%", top: " 35.8%" },
+    { left: "44.5%", top: " 41.8%" },
+    { left: "44.5%", top: " 47.3%" },
+    { left: "44.5%", top: " 53.3%" },
+    { left: "44.5%", top: " 59.1%" },
+    { left: "44.5%", top: " 64.8%" },
+  ],
+  [
+    { left: "56.5%", top: " 35.8%" },
+    { left: "56.5%", top: " 41.8%" },
+    { left: "56.5%", top: " 47.3%" },
+    { left: "56.5%", top: " 53.3%" },
+    { left: "56.5%", top: " 59.1%" },
+    { left: "56.5%", top: " 64.8%" },
+  ],
+  [
+    { left: "69.2%", top: " 35.8%" },
+    { left: "69.2%", top: " 41.8%" },
+    { left: "69.2%", top: " 47.3%" },
+    { left: "69.2%", top: " 53.3%" },
+    { left: "69.2%", top: " 59.1%" },
+    { left: "69.2%", top: " 64.8%" },
+  ],
+  [
+    { left: "81.5%", top: " 35.8%" },
+    { left: "81.5%", top: " 41.8%" },
+    { left: "81.5%", top: " 47.3%" },
+    { left: "81.5%", top: " 53.3%" },
+    { left: "81.5%", top: " 59.1%" },
+    { left: "81.5%", top: " 64.8%" },
+  ],
+];
+
+let large = document.getElementById("counter");
+let small = document.getElementById("counter1");
+let game = document.getElementsByClassName("flex-conter-board");
+
+function animate(element, frames) {
+  element.animate(frames, {
+    duration: 1000,
+    iterations: 1,
+    fill: "forwards",
+  });
+}
+
+function checkScreenSize() {
+  let screen = "";
+
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    screen = "mobile";
   }
-    
-  function checkScreenSize() {
+  if (window.matchMedia("(min-width: 768px) and (max-width: 1439px)").matches) {
+    screen = "tablet";
+  }
+  if (window.matchMedia("(min-width: 1440px)").matches) {
+    screen = "desktop";
+  }
 
-   let screen="";
-
-   if(window.matchMedia("(max-width: 767px)").matches)
-   {
-    screen="mobile"
-   }
-   if(window.matchMedia("(min-width: 768px) and (max-width: 1439px)").matches) {
-    screen="tablet"
-   }
-   if(window.matchMedia("(min-width: 1440px)").matches)
-   {
-    screen="desktop"
-   }
-
-    return screen;
-  };
-
-
- 
+  return screen;
+}
 
 function animateOnScreen() {
-  switch(checkScreenSize())
-  {
-      case "mobile":
-              animate(small,mobileAnimation[0,0]);
+  switch (checkScreenSize()) {
+    case "mobile":
+      animate(small, mobileAnimation[(0, 0)]);
       break;
-      case "tablet":
-              animate(large,tabletAnimation[0,0]);
+    case "tablet":
+      animate(large, tabletAnimation[(0, 0)]);
       break;
-      case "desktop":
-              animate(large,desktopAnimation[0,0]);
+    case "desktop":
+      animate(large, desktopAnimation[(0, 0)]);
       break;
-  
   }
 }
 
-const drops = document.querySelectorAll('.droppoint');
-const counters = document.querySelectorAll('.counters div');
+/*
+ function create a counter elements and set them on the screen 
+*/
+function createCounters(quantity = 1,className,prefix) {
+  let location = document.querySelector(".counters");
+  for (let i = 1; i < quantity + 1; i++) {
+    let element = document.createElement("div");
+    element.id = prefix+i;
+    element.className = className;
+    element.draggable = true;
+    location.appendChild(element);
+  }
+}
 
-drops.forEach(droppoint=> {
-  droppoint.addEventListener('dragenter', dragEnter)
-  droppoint.addEventListener('dragover', dragOver);
-  droppoint.addEventListener('dragleave', dragLeave);
-  droppoint.addEventListener('drop', drop);
-})
-counters.forEach(counter=> {
-  counter.addEventListener('dragstart',dragStart);
-})
+createCounters(20,"counter-red-large","cl0");
 
+/*
+ function create a counter elements and set them on the screen 
+*/
+
+const drops = document.querySelectorAll(".droppoint");
+const counters = document.querySelectorAll(".counters div");
+
+drops.forEach((droppoint) => {
+  droppoint.addEventListener("dragenter", dragEnter);
+  droppoint.addEventListener("dragover", dragOver);
+  droppoint.addEventListener("dragleave", dragLeave);
+  droppoint.addEventListener("drop", drop);
+});
+counters.forEach((counter) => {
+  counter.addEventListener("dragstart", dragStart);
+});
 
 function dragStart(e) {
-  e.dataTransfer.setData('text/plain',e.target.id);
-  console.log(e.target.id);
+  e.dataTransfer.setData("text/plain", e.target.id);
+  
 }
 
 function dragEnter(e) {
   e.preventDefault();
-  console.log(e.target);
+  
 }
 
 function dragOver(e) {
   e.preventDefault();
-  e.target.classList.add('marker-red-large');
-  console.log(e.target);
+  e.target.classList.add("marker-red-large");
+
 }
 
 function dragLeave(e) {
   e.preventDefault();
-  e.target.classList.remove('marker-red-large');
+  e.target.classList.remove("marker-red-large");
 }
 
 function drop(e) {
-  
-  const id=e.dataTransfer.getData('text/plain');
+  const id = e.dataTransfer.getData("text/plain");
   const draggable = document.getElementById(id);
-  e.target.classList.remove('marker-red-large');
-  animate(draggable,desktopAnimation[0,parseInt(e.target.id)]);
+  e.target.classList.remove("marker-red-large");
+  animate(draggable, desktopAnimation[(0, parseInt(e.target.id))]);
  
-  
 }
-
-
-
-
-
-
