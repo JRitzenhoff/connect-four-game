192636a545615ef0c9e4eb3242208ad59433cd13
kml <kml.ptb@gmail.com>
2022-11-06 23:52:56 +0000

    Touch screen support added

--- js/test.js
+++ js/test.js
@@ -183,13 +183,47 @@ const gameArray = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
 ];
-
-
-
-
+let desktopMarker = [
+  "420px",
+  "510px",
+  "600px",
+  "690px",
+  "770px",
+  "860px",
+  "950px",
+];
+let mobileMarker = [
+  "25px",
+  "75px",
+  "120px",
+  "170px",
+  "215px",
+  "260px",
+  "310px",
+];
+let tabletMarker = [
+  "95px",
+  "185px",
+  "270px",
+  "355px",
+  "450px",
+  "540px",
+  "630px",
+];
 let large = document.getElementById("counter");
 let small = document.getElementById("counter1");
 let game = document.getElementsByClassName("flex-conter-board");
+let board = document.getElementById("3");
+let markers =  document.querySelectorAll(".markers div");
+let markerRedSmall = document.querySelector(".marker-red-small");
+let markerRedLarge = document.querySelector(".marker-red-large");
+let markerYellowSmall = document.querySelector(".marker-yellow-small");
+let markerYellowLarge = document.querySelector(".marker-yellow-large");
+
+let gameboard = document.querySelector(".game-container");
+let screen = document.querySelector(".screen");
+let gameResult=document.querySelector(".game-result");
+let turn=document.querySelector(".turn-red");
 
 const drops = document.querySelectorAll(".droppoint");
 const counters = document.querySelectorAll(".counters div");
@@ -198,8 +232,42 @@ const countersTablet = document.querySelector(".game-counters .tablet");
 const countersDesktop = document.querySelector(".game-counters .desktop");
 
 const countersMobileAll = document.querySelectorAll(".counters .mobile");
-const countersTabletAll = document.querySelectorAll("counters .tablet");
+const countersTabletAll = document.querySelectorAll(".counters .tablet");
 const countersDesktopAll = document.querySelectorAll(".counters .desktop");
+
+const MobileAllCounters = document.querySelectorAll(".counters .mobile div");
+const MobileAllRed = document.querySelectorAll(
+  ".counters .mobile .counter-red-small"
+);
+const MobileAllYellow = document.querySelectorAll(
+  ".counters .mobile .counter-yellow-small"
+);
+
+const TabletAllCounters = document.querySelectorAll(".counters .tablet div");
+
+const TabletAllRed = document.querySelectorAll(
+  ".counters .tablet .counter-red-large"
+);
+const TabletAllYellow = document.querySelectorAll(
+  ".counters .tablet .counter-yellow-large"
+);
+
+const DesktopAllCounters = document.querySelectorAll(".counters .desktop div");
+const DesktopAllRed = Array.from(
+  document.querySelectorAll(".counters .desktop .counter-red-large")
+);
+const DesktopAllYellow = Array.from(
+  document.querySelectorAll(".counters .desktop .counter-yellow-large")
+);
+let tabletArray = Array.from(TabletAllCounters);
+
+let drop0 = document.getElementById("0");
+let drop1 = document.getElementById("1");
+let drop2 = document.getElementById("2");
+let drop3 = document.getElementById("3");
+let drop4 = document.getElementById("4");
+let drop5 = document.getElementById("5");
+let drop6 = document.getElementById("6");
 
 function animate(element, frames) {
   if (frames.length === 1) {
@@ -254,7 +322,7 @@ function animateOnScreen(element, frameNumber) {
     case "desktop":
       // setDisplayCounters(countersTabletAll);
       // setDisplayCounters(countersMobileAll);
-     
+
       animate(element, desktopAnimation[frameNumber]);
       savePlayerMove(element, desktopAnimation[frameNumber], frameNumber);
       break;
@@ -275,30 +343,25 @@ function setCounterPosition(element, frameNumber) {
       case "desktop":
         newElement.style.left = desktop.left;
         newElement.style.top = desktop.top;
-        moveCounterToGameBoard(newElement,"desktop");
+        moveCounterToGameBoard(newElement, "desktop");
         break;
       case "tablet":
         newElement.style.left = tablet.left;
         newElement.style.top = tablet.top;
-        moveCounterToGameBoard(newElement,"tablet");
+        moveCounterToGameBoard(newElement, "tablet");
         break;
       case "mobile":
         newElement.style.left = mobile.left;
         newElement.style.top = mobile.top;
-        moveCounterToGameBoard(newElement,"mobile");
+        moveCounterToGameBoard(newElement, "mobile");
         break;
     }
-    
   });
-
- 
 }
 
-function moveCounterToGameBoard(element,boardElement)
-{
-  let board = document.querySelector(".game-counters ."+boardElement);
+function moveCounterToGameBoard(element, boardElement) {
+  let board = document.querySelector(".game-counters ." + boardElement);
   board.appendChild(element);
-
 }
 
 drops.forEach((droppoint) => {
@@ -313,88 +376,267 @@ counters.forEach((counter) => {
 
 function dragStart(e) {
   e.dataTransfer.setData("text/plain", e.target.id);
+  
 }
 
 function dragEnter(e) {
   e.preventDefault();
+
+
+let color=e.dataTransfer.getData("text/plain").split('-')[1];
+  let target = e.target.id;
+  showMarker(color, target)
+ 
 }
 
 function dragOver(e) {
   e.preventDefault();
-
-  //e.target.classList.add("marker-red-large");
 }
 
 function dragLeave(e) {
   e.preventDefault();
-  //e.target.classList.remove("marker-red-large");
 }
 
 function drop(e) {
+  
   const id = e.dataTransfer.getData("text/plain");
   const dragElement = document.getElementById(id);
   let color = getCounterColor(dragElement);
   animateOnScreen(dragElement, parseInt(e.target.id));
 
-  checkWinner(color);
+  
 }
 
-function savePlayerMove(element, animationFrameArr, position) {
+var activeEvent = "";
+var originalX = "";
+var originalY = "";
+
+// drops.forEach((droppoint) => {
+//   droppoint.addEventListener("touchstart", TouchStart);
+
+// });
+
+counters.forEach((counter) => {
+  counter.addEventListener("touchstart", TouchStart);
+  counter.addEventListener("touchmove", TouchMove);
+  counter.addEventListener("touchend", TouchEnd);
+});
+
+function TouchStart(e) {
+  originalX = e.target.offsetLeft + "px";
+  originalY = e.target.offsetTop + "px";
+
+  activeEvent = "start";
+}
+
+function TouchMove(e) {
+  e.preventDefault();
+  let width = parseInt(window.getComputedStyle(e.target).width);
+  let height = parseInt(window.getComputedStyle(e.target).height);
+  var touchLocation = e.targetTouches[0];
+  let  color=touchLocation.target.id.split("-")[0];
+  var pageX = touchLocation.pageX - 50 + "px";
+  var pageY = touchLocation.pageY - 50 + "px";
+  let pageXX = parseInt(e.target.style.left);
+  let pageYY = parseInt(e.target.style.top);
+  e.target.style.zIndex = 100;
+  e.target.style.position = "absolute";
+  e.target.style.left = pageX;
+  e.target.style.top = pageY;
+  activeEvent = "move";
+  if (detectTouchEnd(pageXX, pageYY, drop0, width, height)) {
+    showMarker(color,0)
+  } else if (detectTouchEnd(pageXX, pageYY, drop1, width, height)) {
+    showMarker(color,1)
+  } else if (detectTouchEnd(pageXX, pageYY, drop2, width, height)) {
+    showMarker(color,2)
+  } else if (detectTouchEnd(pageXX, pageYY, drop3, width, height)) {
+    showMarker(color,3)
+  } else if (detectTouchEnd(pageXX, pageYY, drop4, width, height)) {
+    showMarker(color,4)
+  } else if (detectTouchEnd(pageXX, pageYY, drop5, width, height)) {
+    showMarker(color,5)
+  } else if (detectTouchEnd(pageXX, pageYY, drop6, width, height)) {
+    showMarker(color,6)
+  }
+  
+}
+
+function TouchEnd(e) {
+  e.preventDefault();
+  e.stopImmediatePropagation();
+  if (activeEvent === "move") {
+    let element = e.target;
+    let pageX = parseInt(e.target.style.left);
+    let pageY = parseInt(e.target.style.top);
+    let width = parseInt(window.getComputedStyle(e.target).width);
+    let height = parseInt(window.getComputedStyle(e.target).height);
+    element.style.zIndex = 10;
+
+    if (detectTouchEnd(pageX, pageY, drop0, width, height)) {
+      animateOnScreen(element, 0);
+    } else if (detectTouchEnd(pageX, pageY, drop1, width, height)) {
+      animateOnScreen(element, 1);
+    } else if (detectTouchEnd(pageX, pageY, drop2, width, height)) {
+      animateOnScreen(element, 2);
+    } else if (detectTouchEnd(pageX, pageY, drop3, width, height)) {
+      animateOnScreen(element, 3);
+    } else if (detectTouchEnd(pageX, pageY, drop4, width, height)) {
+      animateOnScreen(element, 4);
+    } else if (detectTouchEnd(pageX, pageY, drop5, width, height)) {
+      animateOnScreen(element, 5);
+    } else if (detectTouchEnd(pageX, pageY, drop6, width, height)) {
+      animateOnScreen(element, 6);
+    } else {
+      e.target.style.left = originalX;
+      e.target.style.top = originalY;
+    }
+  }
+}
+
+function detectTouchEnd(pageX, pageY, drop, width, height) {
+  if (
+    pageX + width / 2 > drop.getBoundingClientRect().left &&
+    pageX + width / 2 < drop.getBoundingClientRect().left + height &&
+    pageY + height / 2 > drop.getBoundingClientRect().top &&
+    pageY + height / 2 < drop.getBoundingClientRect().top + height
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function showMarker(color, target) {
+
+  console.log(color);
+       let  markerSmall=markerRedSmall;
+        let  markerLarge=markerRedLarge;
+        if(color === "yellow") {
+          markerSmall=markerYellowSmall;
+          markerLarge=markerYellowLarge;
+        }
+      
+      if (checkScreenSize() === "mobile") {
+        markerSmall.style.position = "absolute";
+        markerSmall.style.left = mobileMarker[target];
+        markerSmall.style.top = "230px";
+        markerSmall.style.display= "block";
+      } else if (checkScreenSize() === "tablet" ) {
+        markerLarge.style.position = "absolute";
+        markerLarge.style.left = tabletMarker[target];
+        markerLarge.style.top = "230px";
+        markerLarge.style.display= "block";
+      }else if (checkScreenSize() === "desktop") {
+        markerLarge.style.position = "absolute";
+        markerLarge.style.left = desktopMarker[target];
+        markerLarge.style.top = "100px";
+        markerLarge.style.display= "block";
+      }
+  
+     
+  }
+
  
-  gameArray[animationFrameArr.length -1][position] = getCounterColor(element);
+
+function hideAllMarkes() {
+  markers.forEach(marker => {
+    marker.style.display = "none";
+  })
+}
+
+hideAllMarkes();
+
+function savePlayerMove(element, animationFrameArr, position) {
+  gameArray[animationFrameArr.length - 1][position] = getCounterColor(element);
 }
 
 function getCounterColor(element) {
   return element.id.split("-")[1];
 }
 
-
-
-function checkMatch(one,two,three,four,color)
-{
-  if(one === two && one === three && one === four && one === color ) {
+function checkMatch(one, two, three, four, color) {
+  if (one === two && one === three && one === four && one === color) {
     return true;
   }
 }
-
+// ---------- game function 
 function checkWinner(color) {
-  for(let column = 0;column <7;column++) {
-    for(let row=0;row<3;row++)
-    {
-      if(checkMatch(gameArray[row][column],gameArray[row+1][column],gameArray[row+2][column],gameArray[row+3][column],color))
-      {
-          console.log("win hotizontal"+color);
+  for (let column = 0; column < 7; column++) {
+    for (let row = 0; row < 3; row++) {
+      if (
+        checkMatch(
+          gameArray[row][column],
+          gameArray[row + 1][column],
+          gameArray[row + 2][column],
+          gameArray[row + 3][column],
+          color
+        )
+      ) {
+       return true;
       }
-      if(checkMatch(gameArray[row][column],gameArray[row][column+1],gameArray[row][column+2],gameArray[row][column+3],color))
-      {
-        console.log("win verticla"+color);
-      }
-     
-    }
-  }
-
-  for(let column=0;column<4;column++)
-  {
-    for(let row=0;row<3;row++)
-    {
-      if(checkMatch(gameArray[row][column],gameArray[row+1][column+1],gameArray[row+2][column+2],gameArray[row+3][column+3],color))
-      {
-        console.log("win diagonal"+color);
-      }
-    }
-  }
-
-  for(let column=0;column<4;column++)
-  {
-    for(let row=5;row>2;row--)
-    {
-      if(checkMatch(gameArray[row][column],gameArray[row-1][column+1],gameArray[row-2][column+2],gameArray[row-3][column+3],color))
-      {
-        console.log("win diagonal 2"+color);
+      if (
+        checkMatch(
+          gameArray[row][column],
+          gameArray[row][column + 1],
+          gameArray[row][column + 2],
+          gameArray[row][column + 3],
+          color
+        )
+      ) {
+        return true;
       }
     }
   }
 
- 
+  for (let column = 0; column < 4; column++) {
+    for (let row = 0; row < 3; row++) {
+      if (
+        checkMatch(
+          gameArray[row][column],
+          gameArray[row + 1][column + 1],
+          gameArray[row + 2][column + 2],
+          gameArray[row + 3][column + 3],
+          color
+        )
+      ) {
+        return true;
+      }
+    }
+  }
+
+  for (let column = 0; column < 4; column++) {
+    for (let row = 5; row > 2; row--) {
+      if (
+        checkMatch(
+          gameArray[row][column],
+          gameArray[row - 1][column + 1],
+          gameArray[row - 2][column + 2],
+          gameArray[row - 3][column + 3],
+          color
+        )
+      ) {
+        return true;
+      }
+    }
+  }
 }
 
+function changeTurn(color)
+{
+  if(color==="red")
+  {
+    turn.style.backgroundImage = "url('../assets/images/turn-background-red.svg')"; 
+    turn.textContent="Player 1's turn";
+  }
+  else {
+    turn.style.backgroundImage = "url('../assets/images/turn-background-yellow.svg')"; 
+    turn.textContent="Player 1's turn";
+  }
+}
+
+function runGame() {
+  gameResult.style.display = "none";
+}
+
+runGame();
