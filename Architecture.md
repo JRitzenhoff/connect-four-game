47dd559be563e095eff48d3aa823ee9d32631a0c
Jan <jp.ritzenhoff@gmail.com>
2024-10-05 22:45:10 +0000

    Added logic to read column values from the commandline, send them to the clients, and log to the browser console on receive

--- Architecture.md
+++ Architecture.md
@@ -2,5 +2,16 @@
 
 To add button feedback to the Frontend-only Javascript application, it made sense to:
 * Add a Node.js Express server to the backend (https://www.digitalocean.com/community/tutorials/nodejs-express-basics)
-* Add a Websocket server (https://cheatcode.co/blog/how-to-set-up-a-websocket-server-with-node-js-and-express)
-* Add a Websocket client (https://cheatcode.co/blog/how-to-set-up-a-websocket-client-with-javascript)
+* Add Server-Side Events to the express server (https://www.digitalocean.com/community/tutorials/nodejs-server-sent-events-build-realtime-app)
+* (Optional Alternative) Add a Websocket client and server (https://dev.to/dipakahirav/real-time-communication-with-websockets-a-complete-guide-32g4)
+
+
+## Research
+
+Investigated a number of options for communicating from the Server to the Client. 
+
+https://medium.com/ableneo/deciding-a-way-to-exchange-data-between-client-and-server-a1ed1b949441
+
+Discovered that the best two options are:
+* Server-Side Events (SSE's) for mono-directional communication from Server to Client
+* Websockets for bidirectional communication between the Server and Client
--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
-// setup the middleware to serve the files in the public directory
+
+const PORT = 3000;
+
+let eventClients = [];
+let columnNumber = 0;
+
+
+// ----------------------------------------
+// Initialize and run the the server
+// ---------------------
 app.use('/play', express.static('public'));
 
 app.get('/', (req, res) => {
@@ -10,4 +19,84 @@ app.get('/', (req, res) => {
     res.redirect('/play')
 });
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(PORT, () => {
+    console.log(`Events service listening at http://localhost:${PORT}`)
+
+    // start asking for commandline prompts
+    askCommandlineForColumn();
+})
+
+
+// ----------------------------------------
+// Logic related to event handling/sending
+// ---------------------
+function formatColumnNumberEvent(number) {
+    // https://web.dev/articles/eventsource-basics
+    return `data: ${columnNumber}\n\n`;
+}
+
+// save the client and send an initial data message
+function registerEventClient(request, response, next) {
+    // keep the connection alive indefinitely (to send events)
+    const headers = {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    };
+    response.writeHead(200, headers);
+
+    // send an initial message with the data value (need specific format)
+    const columnNumberEvent = formatColumnNumberEvent(columnNumber);
+    response.write(columnNumberEvent);
+
+    // save the client (connection stays open because of the 'keep-alive')
+    const clientId = Date.now();
+    const newClient = {
+        id: clientId,
+        response
+    };
+    eventClients.push(newClient);
+
+    // callback to remove the client once the client's connection has closed
+    request.on('close',
+        () => {
+            console.log(`${clientId} Connection closed`);
+            eventClients = eventClients.filter(client => client.id !== clientId);
+        });
+}
+
+app.get('/receive_events', registerEventClient);
+
+// Function that writes to the active eventClients
+function sendPlacedColumnEvent(columnNumber) {
+    const updatedColumn = formatColumnNumberEvent(columnNumber);
+
+    eventClients.forEach(client =>
+        client.response.write(updatedColumn)
+    );
+}
+
+// ----------------------------------------
+// Read column value from commandline
+// ---------------------
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+// rl.on('close', () => { console.log('Shutting down prompts') })
+
+const askCommandlineForColumn = () => {
+    rl.question(`Which column was a chip placed?: `,
+        selectedColumnString => {
+            let selectedColumn = parseInt(selectedColumnString, radix = 10)
+            console.log(`Column selected ${selectedColumn}!\n`);
+
+            sendPlacedColumnEvent(selectedColumn)
+
+            // call the same function recursively (until a break happens)
+            askCommandlineForColumn();
+        })
+}
+
